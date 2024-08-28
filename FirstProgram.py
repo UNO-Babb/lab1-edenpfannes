47f6a5698fdbc4adc362f561ad2b1eac90314537
edenpfannes <epfannes@unomaha.edu>
2024-08-28 16:46:48 +0000

    Second Update

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,14 +6,13 @@
 def main():
   print("First Program")
   #Say hello
-  print("Hello, how are you?")
-  
+  input("Hello, how are you?: ")
   #Ask for the user's name
   name = input("What is your name?: ")
   #Use the user's name in the program.
   print("Good to meet you", name)
   #Ask the user for their age.
-  age = input("How old are you?" )
+  age = input("How old are you?: ")
   print("Wow, you are old!")
   #Tell the user what year they were born in.
   age = int(age)
