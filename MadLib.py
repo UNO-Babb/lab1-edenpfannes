4222591e2e9eab9763df770bf277b1b0576cb565
edenpfannes <epfannes@unomaha.edu>
2024-08-30 11:51:48 -0500

    Update MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -1,7 +1,7 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Eden Pfannes
+#Date: 8/30/24
+#Assignment: Editing Lab #1
 
 def main():
   print("Madlib")
@@ -11,8 +11,13 @@ def main():
   noun3 = input("Enter a noun: ")
   name1 = input("Enter a name: ")
   verb1 = input("Enter a verb: ")
+  verb2 = input("Enter a verb: ")
   #Print the story with the user supplied words.
-
+  noun1 = input("Once upon a time there was a: ") 
+  noun2 = input("with a very large: ")
+  name1 = input("His name was: ")
+  name1 = int(name)
+  verb1 = input("name1 "was very excited to: ")
 
 
 #Call the main function if this is the file being run.
